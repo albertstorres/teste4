4d96d8f79c525d8e762511561e01f55cd54182b2
albertstorres <albertstorres@gmail.com>
2023-07-07 20:36:14 -0300

    QUARTO TESTE.

---
+++ teste4.js
@@ -0,0 +1,10 @@
+let numero = 5;
+let fatorial = numero;
+let aux = numero;
+
+while (numero > 1) {
+    fatorial = fatorial * (numero - 1);
+    numero--;
+}
+
+console.log("O FATORIAL DE ", aux, " EH = ", fatorial);
